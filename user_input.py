ebd4408a8882e075c008a1821e7fb0fd8e7303a6
Nielsen015 <mosesnielsen68@gmail.com>
2024-11-05 23:31:43 +0300

    Week 1  assignment

--- user_input.py
+++ user_input.py
@@ -3,5 +3,4 @@ name = input("Please enter your name: ")
 age = input("Please enter your age: ")
 location = input("Please enter your location: ")
 
-# Print out a personalized message
 print(f"Hello {name}, you are {age} years old and live in {location}.")
